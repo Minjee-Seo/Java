c0da98d5548e171cd226ba62be533452a4d5dd2d
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-11 19:07:33 +0900

    Create List.java

---
+++ 03/List.java
@@ -0,0 +1,34 @@
+// ArrayList
+
+import java.util.ArrayList;
+
+public class Sample {
+    public static void main(String[] args) {
+        ArrayList<string> coffee = new ArrayList<>();
+        pitches.add("espresso");
+        pitches.add("americano");
+        pitches.add("latte");
+    }
+}
+
+// 특정 위치에 삽입하고 싶은 경우
+
+coffee.add(0, "espresso");
+
+// 해당 인덱스의 요소 가져오기 : get
+
+coffee.get(0);
+
+// 요소 개수 세기 : size
+
+coffee.size();
+
+// contains : 해당 리스트에 항목 있는지.
+
+coffe.contains("americano"); // true
+
+// remove : 요소 삭제
+
+coffee.remove(0); // 인덱스로, 리턴값 : 삭제된 항목
+coffee.remove("espresso"); // 이름으로, 리턴값 : true / false
+
