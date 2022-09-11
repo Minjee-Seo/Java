522cc8be53d5bc2d68dea5e110b08df00d713dfd
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-11 21:03:25 +0900

    Create Map.java

---
+++ 03/Map.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Sample {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("people", "사람");
+        map.put("baseball", "야구");
+    }
+}
+
+System.out.println(map.get("people"));
+
+System.out.println(map.containsKey("people"));
+
+System.out.println(map.remove("people"));
+
+System.out.println(map.size());
+
+import java.util.HashMap;
+
+public class Sample {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("people", "사람");
+        map.put("baseball", "야구");
+        System.out.println(map.keySet());  // [baseball, people] 출력
+    }
+}
