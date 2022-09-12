2e9746fbfb7ba07c31aceb55d5438d3bc48543a8
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-12 23:36:52 +0900

    Create Set.java

---
+++ 03/Set.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+    }
+}
+
