76437299030b6c33ac89520556c386c3b83ea96a
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-13 22:18:48 +0900

    Update Set.java

--- 03/Set.java
+++ 03/Set.java
@@ -1,10 +1,80 @@
+/* Set : 집합 자료형
+
+중복을 허용하지 않음, 순서 존재 x -> 인덱싱 통해 값 얻을 수 없음. */
+
 import java.util.Arrays;
 import java.util.HashSet;
+
+// retainAll : 교집합 구하기
 
 public class Sample {
     public static void main(String[] args) {
         HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+        
+        HashSet<Integer> intersection = new HashSet<>(s1);
+        
+        intersection.retainAll(s2); // intersection의 내용이 교집합으로 변경됨.
+        
+        System.out.println(intersection); // [4, 5, 6] 출력
     }
 }
 
+// addAll : 합집합 구하기
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+
+        HashSet<Integer> union = new HashSet<>(s1);  // s1으로 union 생성
+        union.addAll(s2); // 합집합 수행
+        System.out.println(union);  // [1, 2, 3, 4, 5, 6, 7, 8, 9] 출력
+    }
+}
+
+// addAll 이용하면 값 여러 개 추가도 가능.
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Jump");
+        set.addAll(Arrays.asList("To", "Java"));
+        System.out.println(set);  // [Java, To, Jump] 출력
+    }
+}
+
+// removeAll : 차집합 구하기
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+
+        HashSet<Integer> substract = new HashSet<>(s1);  // s1으로 substract 생성
+        substract.removeAll(s2); // 차집합 수행
+        System.out.println(substract);  // [1, 2, 3] 출력
+    }
+}
+
+// Add : 값 추가하기
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Jump");
+        set.add("To");
+        set.add("Java");
+        System.out.println(set);  // [Java, To, Jump] 출력
+    }
+}
+
+// remove : 특정 값 제거하기
+
+public class Sample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>(Arrays.asList("Jump", "To", "Java"));
+        set.remove("To");
+        System.out.println(set);  // [Java, Jump] 출력
+    }
+}
