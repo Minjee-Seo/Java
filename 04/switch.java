1512ac497e3690758eccf42033cefba414716134
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-14 15:22:40 +0900

    Create switch.java

---
+++ 04/switch.java
@@ -0,0 +1,17 @@
+// switch, case문
+
+public class Sample {
+  public static void main(string[] args) {
+    int num = 3;
+    String menu = "";
+    
+    switch (num) {
+      case 1 : menu = "espresso"; break;
+      case 2 : menu = "americano"; break;
+      case 3 : menu = "latte"; break;
+    }
+    
+    System.out.println(menu);
+    
+  }
+}
