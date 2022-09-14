f4354754390427ff235c98f40fdf23fc25c14ece
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-14 15:16:06 +0900

    Create if.java

---
+++ 04/if.java
@@ -0,0 +1,21 @@
+// if, else if, else
+
+// 예시
+
+boolean money = true;
+
+if (money) {
+  System.out.println("돈이 있음") }
+
+else {
+  System.out.prinln("돈 없음") }
+
+// List 자료형을 이용한 예시
+
+ArrayList<String> pocket = new ArrayList<>();
+
+pocket.add("paper");
+pocket.add("money");
+
+if (pocket.contains("money")) System.out.println("돈 있음");
+else System.out.println("돈 없음");
